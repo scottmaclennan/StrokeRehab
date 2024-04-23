fd1fc7ea950ae21bffa3fc12b9b8ccf6aefa563a
scottmaclennan <scottmaclennan30@gmail.com>
2024-04-23 10:30:05 +0100

    added home screen

--- StrokeRehab/App.js
+++ StrokeRehab/App.js
@@ -1,31 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import * as Notifications from 'expo-notifications';
 import { StyleSheet, Text, View } from 'react-native';
-
-const Stack = createNativeStackNavigator();
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="AppointmentDetails" component={AppointmentDetailScreen} />
-    </Stack.Navigator>
-  );
+  return <AppNavigator />;
 }
 
-async function scheduleNotification() {
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: "Time to take your medicine",
-      body: 'Take 1 tablet of XYZ',
-    },
-    trigger: {
-      hour: 9,
-      minute: 0,
-      repeats: true
-    },
-  });
-}
+
 
 const styles = StyleSheet.create({
   container: {
