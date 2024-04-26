8843946a790574f536d3c2758cd80339fa763205
scottmaclennan <scottmaclennan30@gmail.com>
2024-04-26 12:40:31 +0100

    updated

--- StrokeRehab/src/navigation/AppNavigator.js
+++ StrokeRehab/src/navigation/AppNavigator.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import MealPlanScreen from '../screens/MealPlanScreen';
-import GroceryListScreen from '../screens/GroceryListScreen';
-import AppointmentDetailScreen from '../screens/AppointmentDetailScreen';
 import ExerciseScreen from '../screens/ExerciseScreen';
 import MedicationScreen from '../screens/MedicationScreen';
-import SymptomDairyScreen from '../screens/SymptomDairyScreen';
-
 
 const Stack = createNativeStackNavigator();
 
@@ -16,11 +12,8 @@ function AppNavigator() {
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="FoodPlanner" component={MealPlanScreen} />
-      <Stack.Screen name="AppointmentDetailScreen" component={AppointmentDetailScreen} />
       <Stack.Screen name="ExercisePlanner" component={ExerciseScreen} />
       <Stack.Screen name="MedicationPlanner" component={MedicationScreen} />
-      <Stack.Screen name="SymptomDairyScreen" component={SymptomDairyScreen} />
-      <Stack.Screen name="GroceryList" component={GroceryListScreen} />
     </Stack.Navigator>
   );
 }
